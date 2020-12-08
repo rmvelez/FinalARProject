9c704e7a94e989b2b6184583706d48ee9d29f40c
Rick Velez <cptrick12191@cox.net>
2020-12-08 01:38:20 -0500

    Uploaded Project
    Uploaded the completed AR Project.

---
+++ ARFinalProject/Assets/Scripts/FoeControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoeControl : MonoBehaviour
+{
+    public GameObject healthUI;
+    private int foeHealth;
+    public Text foeHealthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foeHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foeHealth <= 0)
+        {
+            this.gameObject.SetActive(false);
+            healthUI.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Friend")
+        {
+            healthUI.SetActive(true);
+            foeHealthText.text = "P2: " + foeHealth;
+        }
+        if (other.gameObject.tag == "GoodHitBox")
+        {
+            foeHealth -= 5;
+            foeHealthText.text = "P2: " + foeHealth;
+        }
+    }
+}
---
+++ ARFinalProject/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject battleUI;
+    private int playerHealth;
+    public Text playerHealthText;
+
+    private bool battleReady;
+    public GameObject hitBox;
+    public GameObject hitBoxSpawn;
+    public float startTimeBetweenHits;
+    private float timeBetweenHits;
+    private float durationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 100;
+        battleReady = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(battleReady == true)
+        {
+            if (timeBetweenHits <= 0)
+            {
+                Instantiate(hitBox, hitBoxSpawn.transform.position, hitBox.transform.rotation);
+                durationTime = 5f;
+                timeBetweenHits = startTimeBetweenHits;
+            }
+            if (durationTime <= 0)
+            {
+                timeBetweenHits -= Time.deltaTime;
+            }
+
+            durationTime -= 1f;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Foe")
+        {
+            battleUI.SetActive(true);
+            playerHealthText.text = "P1: " + playerHealth;
+            battleReady = true;
+        }
+    }
+}
